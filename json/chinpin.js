927eb170d30ae1f0cff43b496c74b6678586f230
“bob”-xxxx” <heavypolysic@gmail.com>
2022-05-03 23:00:24 +0900

    珍品検索を追加

---
+++ json/chinpin.js
@@ -0,0 +1,865 @@
+const chinpinJson = {
+    "玉の彫刻" : {
+        "max": 20,
+        "openLevel": 7,
+        "time": 12,
+        "busyoRank": "UR",
+        "busyo": [
+            "全UR"
+        ],
+        "child": [
+            {
+                "name":"五銖銭",
+                "required": 6
+            }
+        ] 
+    },
+    "錬丹書典" : {
+        "max": 20,
+        "openLevel": 7,
+        "time": 12,
+        "busyoRank": "UR",
+        "busyo": [
+            "于吉",
+            "左慈"
+        ],
+        "child": [
+            {
+                "name":"象牙",
+                "required": 30
+            },
+            {
+                "name":"玉器",
+                "required": 30
+            }
+        ] 
+    },
+    "良馬" : {
+        "max": 20,
+        "openLevel": 7,
+        "time": 12,
+        "busyoRank": "UR",
+        "busyo": [
+            "呂布",
+            "関羽",
+            "張飛",
+            "曹操",
+            "孫堅",
+            "項羽",
+        ],
+        "child": [
+            {
+                "name":"象牙",
+                "required": 25
+            },
+            {
+                "name":"琉金釦",
+                "required": 35
+            }
+        ] 
+    },
+    "古い琴" : {
+        "max": 20,
+        "openLevel": 7,
+        "time": 12,
+        "busyoRank": "UR",
+        "busyo": [
+            "諸葛亮",
+            "司馬懿"
+        ],
+        "child": [
+            {
+                "name":"犀皮",
+                "required": 28
+            },
+            {
+                "name":"珠玉",
+                "required": 32
+            }
+        ] 
+    },
+    "弓孥" : {
+        "max": 20,
+        "openLevel": 7,
+        "time": 12,
+        "busyoRank": "UR",
+        "busyo": [
+            "霍去病"
+        ],
+        "child": [
+            {
+                "name":"犀皮",
+                "required": 35
+            },
+            {
+                "name":"玉器",
+                "required": 25
+            }
+        ]
+    },
+    "金剣" : {
+        "max": 20,
+        "openLevel": 7,
+        "time": 12,
+        "busyoRank": "UR",
+        "busyo": [
+            "孫権",
+            "劉備"
+        ],
+        "child": [
+            {
+                "name":"犀皮",
+                "required": 20
+            },
+            {
+                "name":"琉金釦",
+                "required": 40
+            }
+        ] 
+    },
+    "書道用具" : {
+        "max": 20,
+        "openLevel": 7,
+        "time": 12,
+        "busyoRank": "UR",
+        "busyo": [
+            "鍾会",
+            "羊祜",
+            "李斯",
+            "張儀"
+        ],
+        "child": [
+            {
+                "name":"珠玉",
+                "required": 22
+            },
+            {
+                "name":"玉器",
+                "required": 38
+            }
+        ] 
+    },
+    "兵法書典" : {
+        "max": 20,
+        "openLevel": 7,
+        "time": 12,
+        "busyoRank": "UR",
+        "busyo": [
+            "韓信",
+            "呉起",
+            "張遼",
+            "王翦",
+            "白起"
+        ],
+        "child": [
+            {
+                "name":"珠玉",
+                "required": 15
+            },
+            {
+                "name":"琉金釦",
+                "required": 45
+            }
+        ] 
+    },
+    "マント" : {
+        "max": 20,
+        "openLevel": 7,
+        "time": 12,
+        "busyoRank": "UR",
+        "busyo": [
+            "陸抗",
+            "陸遜"
+        ],
+        "child": [
+            {
+                "name":"玉器",
+                "required": 50
+            },
+            {
+                "name":"琉金釦",
+                "required": 10
+            }
+        ] 
+    },
+    "虎符" : {
+        "max": 20,
+        "openLevel": 2,
+        "time": 10,
+        "busyoRank": "SSR",
+        "busyo": [
+            "全SSR"
+        ],
+        "child": [
+            {
+                "name":"象牙",
+                "required": 3
+            },
+            {
+                "name":"珠玉",
+                "required": 2
+            }
+        ] 
+    },
+    "長槍" : {
+        "max": 20,
+        "openLevel": 2,
+        "time": 10,
+        "busyoRank": "SSR",
+        "busyo": [
+            "趙雲",
+            "馬超",
+            "孫策",
+            "夏侯惇",
+            "文醜"
+        ],
+        "child": [
+            {
+                "name":"塗料",
+                "required": 58
+            },
+            {
+                "name":"銅帯鉤",
+                "required": 52
+            }
+        ] 
+    },
+    "研墨" : {
+        "max": 20,
+        "openLevel": 2,
+        "time": 10,
+        "busyoRank": "SSR",
+        "busyo": [
+            "郭嘉",
+            "程昱",
+            "荀彧",
+            "賈詡",
+            "陳宮",
+            "龐統",
+            "徐庶",
+            "張昭",
+            "魯粛"
+        ],
+        "child": [
+            {
+                "name":"紙",
+                "required": 45
+            },
+            {
+                "name":"銅鏡",
+                "required": 75
+            }
+        ] 
+    },
+    "燕脂" : {
+        "max": 20,
+        "openLevel": 2,
+        "time": 10,
+        "busyoRank": "SSR",
+        "busyo": [
+            "貂蝉",
+            "鄒氏",
+            "大喬",
+            "小喬"
+        ],
+        "child": [
+            {
+                "name":"茶葉",
+                "required": 60
+            },
+            {
+                "name":"塗料",
+                "required": 50
+            }
+        ] 
+    },
+    "銀櫛" : {
+        "max": 20,
+        "openLevel": 2,
+        "time": 10,
+        "busyoRank": "SSR",
+        "busyo": [
+            "孫尚香",
+            "馬雲祿",
+            "黄月英"
+        ],
+        "child": [
+            {
+                "name":"朱砂",
+                "required": 45
+            },
+            {
+                "name":"絹布",
+                "required": 65
+            }
+        ] 
+    },
+    "美酒" : {
+        "max": 20,
+        "openLevel": 2,
+        "time": 10,
+        "busyoRank": "SSR",
+        "busyo": [
+            "顔良",
+            "黄忠",
+            "甘寧",
+            "呂蒙",
+            "曹仁",
+            "周泰",
+            "鄧艾",
+            "龐徳"
+        ],
+        "child": [
+            {
+                "name":"紙",
+                "required": 55
+            },
+            {
+                "name":"茶葉",
+                "required": 55
+            }
+        ] 
+    },
+    "盟主令" : {
+        "max": 20,
+        "openLevel": 2,
+        "time": 10,
+        "busyoRank": "SSR",
+        "busyo": [
+            "袁紹",
+            "公孫瓚",
+            "馬騰"
+        ],
+        "child": [
+            {
+                "name":"羅漢杖",
+                "required": 70
+            },
+            {
+                "name":"朱砂",
+                "required": 40
+            }
+        ] 
+    },
+    "錦袍" : {
+        "max": 20,
+        "openLevel": 2,
+        "time": 10,
+        "busyoRank": "SSR",
+        "busyo": [
+            "許褚",
+            "張郃",
+            "夏侯淵",
+            "徐晃"
+        ],
+        "child": [
+            {
+                "name":"銅帯鉤",
+                "required": 45
+            },
+            {
+                "name":"紙",
+                "required": 65
+            }
+        ] 
+    },
+    "双戟" : {
+        "max": 20,
+        "openLevel": 2,
+        "time": 10,
+        "busyoRank": "SSR",
+        "busyo": [
+            "典韋",
+            "太史慈"
+        ],
+        "child": [
+            {
+                "name":"銅帯鉤",
+                "required": 60
+            },
+            {
+                "name":"羅漢杖",
+                "required": 40
+            }
+        ] 
+    },
+    "琴譜" : {
+        "max": 20,
+        "openLevel": 2,
+        "time": 10,
+        "busyoRank": "SSR",
+        "busyo": [
+            "周瑜",
+            "姜維"
+        ],
+        "child": [
+            {
+                "name":"絹布",
+                "required": 60
+            },
+            {
+                "name":"銅鏡",
+                "required": 60
+            }
+        ] 
+    },
+    "銀の鎧" : {
+        "max": 20,
+        "openLevel": 2,
+        "time": 10,
+        "busyoRank": "SSR",
+        "busyo": [
+            "高順",
+            "魏延",
+            "黄蓋"
+        ],
+        "child": [
+            {
+                "name":"絹布",
+                "required": 55
+            },
+            {
+                "name":"紙",
+                "required": 45
+            }
+        ] 
+    },
+    "秦古書" : {
+        "max": 20,
+        "openLevel": 2,
+        "time": 10,
+        "busyoRank": "SSR",
+        "busyo": [
+            "蕭何"
+        ],
+        "child": [
+            {
+                "name":"茶葉",
+                "required": 70
+            },
+            {
+                "name":"銅鏡",
+                "required": 50
+            }
+        ] 
+    },
+    "南華典籍" : {
+        "max": 20,
+        "openLevel": 2,
+        "time": 10,
+        "busyoRank": "SSR",
+        "busyo": [
+            "張角"
+        ],
+        "child": [
+            {
+                "name":"銅鏡",
+                "required": 62
+            },
+            {
+                "name":"羅漢杖",
+                "required": 48
+            }
+        ] 
+    },
+    "名刀" : {
+        "max": 20,
+        "openLevel": 2,
+        "time": 10,
+        "busyoRank": "SSR",
+        "busyo": [
+            "董卓"
+        ],
+        "child": [
+            {
+                "name":"茶葉",
+                "required": 65
+            },
+            {
+                "name":"羅漢杖",
+                "required": 55
+            }
+        ] 
+    },
+    "南蛮象" : {
+        "max": 20,
+        "openLevel": 2,
+        "time": 10,
+        "busyoRank": "SSR",
+        "busyo": [
+            "孟獲"
+        ],
+        "child": [
+            {
+                "name":"銅鏡",
+                "required": 30
+            },
+            {
+                "name":"銅帯鉤",
+                "required": 70
+            }
+        ] 
+    },
+    "医典" : {
+        "max": 20,
+        "openLevel": 2,
+        "time": 10,
+        "busyoRank": "SSR",
+        "busyo": [
+            "華佗"
+        ],
+        "child": [
+            {
+                "name":"塗料",
+                "required": 35
+            },
+            {
+                "name":"銅鏡",
+                "required": 65
+            }
+        ] 
+    },
+    "将軍印" : {
+        "max": 20,
+        "openLevel": 2,
+        "time": 10,
+        "busyoRank": "SSR",
+        "busyo": [
+            "蒙恬"
+        ],
+        "child": [
+            {
+                "name":"朱砂",
+                "required": 68
+            },
+            {
+                "name":"銅帯鉤",
+                "required": 52
+            }
+        ] 
+    },
+    "詩集" : {
+        "max": 20,
+        "openLevel": 2,
+        "time": 10,
+        "busyoRank": "SSR",
+        "busyo": [
+            "曹丕"
+        ],
+        "child": [
+            {
+                "name":"羅漢杖",
+                "required": 50
+            },
+            {
+                "name":"紙",
+                "required": 50
+            }
+        ] 
+    },
+    "五銖銭" : {
+        "max": 100,
+        "openLevel": 7,
+        "time": 8,
+        "busyo": [
+        ],
+        "child": [
+            {
+                "name":"象牙",
+                "required": 6
+            },
+            {
+                "name":"犀皮",
+                "required": 4
+            }
+        ] 
+    },
+    "象牙" : {
+        "max": 100,
+        "openLevel": 7,
+        "time": 6,
+        "busyo": [],
+        "child": [
+            {
+                "name":"塗料",
+                "required": 10
+            },
+            {
+                "name":"朱砂",
+                "required": 10
+            }
+        ] 
+    },
+    "犀皮" : {
+        "max": 100,
+        "openLevel": 7,
+        "time": 6,
+        "busyo": [],
+        "child": [
+            {
+                "name":"絹布",
+                "required": 15
+            },
+            {
+                "name":"塗料",
+                "required": 5
+            }
+        ] 
+    },
+    "珠玉" : {
+        "max": 100,
+        "openLevel": 7,
+        "time": 6,
+        "busyo": [],
+        "child": [
+            {
+                "name":"茶葉",
+                "required": 9
+            },
+            {
+                "name":"絹布",
+                "required": 11
+            }
+        ] 
+    },
+    "玉器" : {
+        "max": 100,
+        "openLevel": 7,
+        "time": 6,
+        "busyo": [],
+        "child": [
+            {
+                "name":"朱砂",
+                "required": 8
+            },
+            {
+                "name":"茶葉",
+                "required": 12
+            }
+        ] 
+    },
+    "琉金釦" : {
+        "max": 100,
+        "openLevel": 7,
+        "time": 6,
+        "busyo": [],
+        "child": [
+            {
+                "name":"紙",
+                "required": 14
+            },
+            {
+                "name":"朱砂",
+                "required": 6
+            }
+        ] 
+    },
+    "塗料" : {
+        "max": 1000,
+        "openLevel": 2,
+        "time": 4,
+        "busyo": [],
+        "child": [
+            {
+                "name":"魚",
+                "required": 9
+            },
+            {
+                "name":"布",
+                "required": 1
+            }
+        ] 
+    },
+    "絹布" : {
+        "max": 1000,
+        "openLevel": 2,
+        "time": 4,
+        "busyo": [],
+        "child": [
+            {
+                "name":"米",
+                "required": 8
+            },
+            {
+                "name":"魚",
+                "required": 2
+            }
+        ] 
+    },
+    "茶葉" : {
+        "max": 1000,
+        "openLevel": 2,
+        "time": 4,
+        "busyo": [],
+        "child": [
+            {
+                "name":"油",
+                "required": 7
+            },
+            {
+                "name":"布",
+                "required": 3
+            }
+        ] 
+    },
+    "朱砂" : {
+        "max": 1000,
+        "openLevel": 2,
+        "time": 4,
+        "busyo": [],
+        "child": [
+            {
+                "name":"布",
+                "required": 6
+            },
+            {
+                "name":"塩",
+                "required": 4
+            }
+        ] 
+    },
+    "紙" : {
+        "max": 1000,
+        "openLevel": 2,
+        "time": 4,
+        "busyo": [],
+        "child": [
+            {
+                "name":"米",
+                "required": 5
+            },
+            {
+                "name":"塩",
+                "required": 5
+            }
+        ] 
+    },
+    "銅帯鉤" : {
+        "max": 1000,
+        "openLevel": 2,
+        "time": 4,
+        "busyo": [],
+        "child": [
+            {
+                "name":"米",
+                "required": 6
+            },
+            {
+                "name":"油",
+                "required": 4
+            }
+        ]
+    },
+    "羅漢杖" : {
+        "max": 1000,
+        "openLevel": 2,
+        "time": 4,
+        "busyo": [],
+        "child": [
+            {
+                "name":"魚",
+                "required": 6
+            },
+            {
+                "name":"塩",
+                "required": 4
+            }
+        ]
+    },
+    "銅鏡" : {
+        "max": 1000,
+        "openLevel": 2,
+        "time": 4,
+        "busyo": [],
+        "child": [
+            {
+                "name":"塩",
+                "required": 8
+            },
+            {
+                "name":"油",
+                "required": 2
+            }
+        ]
+    },
+    "米" : {
+        "max": 9999,
+        "openLevel": 2,
+        "time": 2,
+        "busyo": [],
+        "child": [
+            {
+                "name":"兵糧",
+                "required": 150
+            },
+            {
+                "name":"木材",
+                "required": 150
+            }
+        ] 
+    },
+    "魚" : {
+        "max": 9999,
+        "openLevel": 2,
+        "time": 2,
+        "busyo": [],
+        "child": [
+            {
+                "name":"木材",
+                "required": 150
+            },
+            {
+                "name":"石材",
+                "required": 150
+            }
+        ] 
+    },
+    "布" : {
+        "max": 9999,
+        "openLevel": 2,
+        "time": 2,
+        "busyo": [],
+        "child": [
+            {
+                "name":"石材",
+                "required": 150
+            },
+            {
+                "name":"鉄鉱",
+                "required": 150
+            }
+        ] 
+    },
+    "塩" : {
+        "max": 9999,
+        "openLevel": 2,
+        "time": 2,
+        "busyo": [],
+        "child": [
+            {
+                "name":"兵糧",
+                "required": 150
+            },
+            {
+                "name":"鉄鉱",
+                "required": 150
+            }
+        ] 
+    },
+    "油" : {
+        "max": 9999,
+        "openLevel": 2,
+        "time": 2,
+        "busyo": [],
+        "child": [
+            {
+                "name":"木材",
+                "required": 150
+            },
+            {
+                "name":"鉄鉱",
+                "required": 150
+            }
+        ] 
+    }
+}
